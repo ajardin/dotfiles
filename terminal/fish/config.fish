4e7269251791f56621cb6cf6b6ba24f088b1f496
Alexandre Jardin <info@ajardin.fr>
2026-09-04 21:40:11 +0200

    Drop two dead fish variables and load Starship only when interactive

--- terminal/fish/config.fish
+++ terminal/fish/config.fish
@@ -1,14 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    /opt/homebrew/bin/starship init fish | source
 end
 
 # System environment variables
-set --export HIST_STAMPS "yyyy-mm-dd"
 set --export LANG "en_US.UTF-8"
 set --export LC_CTYPE "UTF-8"
 
 # Composer environment variables
-set --export COMPOSE_HTTP_TIMEOUT 3600
 set --export COMPOSER_MEMORY_LIMIT -1
 
 # AWS environment variables
@@ -19,8 +17,6 @@ eval (/opt/homebrew/bin/brew shellenv)
 fish_add_path --prepend "$HOME/.local/bin"
 fish_add_path --prepend "$HOME/.composer/vendor/bin"
 fish_add_path --prepend "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-
-/opt/homebrew/bin/starship init fish | source
 
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"
